6cc54932a98de7e856b9be17af9d7ec97c2a7338
JoaoGuizelini <jggfranchello@gmail.com>
2026-03-19 18:29:56 -0300

    so arrumando cagadas

--- aula do gil 5/back.js
+++ aula do gil 5/back.js
@@ -4,6 +4,7 @@ const flexDirection = document.getElementById("fd");
 flexDirection.addEventListener("change", function(){
     main.style.flexDirection = this.value;
 });
+
 const justify = document.getElementById("jc");
 
 justify.addEventListener("change", function(){
@@ -11,9 +12,10 @@ justify.addEventListener("change", function(){
 });
 const align = document.getElementById("ai");
 
-align.addEventListener("chenge", function(){
+align.addEventListener("change", function(){
     main.style.alignItems = this.value;
 });
+
 const bola = document.querySelector(".bola");
 const quadrado = document.querySelector(".quadrado");
 
@@ -23,9 +25,26 @@ bola.addEventListener("click", function(){
         box.style.borderRadius = "50%";
     });
 });
+
 quadrado.addEventListener("click", function(){
     const caixas = document.querySelectorAll("#main div");
     caixas.forEach(function(box){
-        box.style.borderRadius = "5px";
+        box.style.borderRadius = "0px";
     })
 });
+
+const sizeInput = document.getElementById("size");
+const div = document.querySelectorAll(".box");
+
+sizeInput.addEventListener("change", function() {
+    div.forEach(function(box){
+        box.style.width = sizeInput.value + "px";
+        box.style.height = sizeInput.value + "px";
+    });
+});
+
+function setBgColor(color) {
+    div.forEach(function(box){
+        box.style.backgroundColor = color;
+    });
+}
